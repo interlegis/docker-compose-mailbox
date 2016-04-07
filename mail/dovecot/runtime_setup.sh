80a9aede2f40cd180060d1453c63c245986c3645
Fabio Rauber <fabiorauber@gmail.com>
2016-04-07 14:16:12 -0300

    Added variable PLAINTEXT_AUTH to allow Dovecot to accept plaintext logins

--- mail/dovecot/runtime_setup.sh
+++ mail/dovecot/runtime_setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 sed -i -e "s/%MAIL_MX_DOMAIN%/${MAIL_MX_DOMAIN}/g" /etc/dovecot/conf.d/15-lda.conf
+
+if [ "${PLAINTEXT_AUTH}" = true ]; then
+  sed -i -e "s/disable_plaintext_auth=yes/disable_plaintext_auth=no/g" /etc/dovecot/conf.d/10-ssl.conf
+  sed -i -e "s/ssl = required//g" /etc/dovecot/conf.d/10-ssl.conf
+fi
