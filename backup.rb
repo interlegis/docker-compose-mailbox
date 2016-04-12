ded6bbc4233d0d1990333be56cd771e2a6e484ee
Fabio Rauber <fabiorauber@gmail.com>
2016-04-12 11:28:38 -0300

    Fixed bug with backup: docker-compose ps is sensitive to terminal width

--- backup.rb
+++ backup.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-storage_box = `docker-compose ps | grep _storagebackup_ | cut -d' ' -f1`.chomp
+storage_box = `docker inspect -f '{{.Name}}' $(docker-compose ps -q)|grep _storagebackup_ | cut -d'/' -f2`.chomp
 exit 1 unless storage_box
 
 puts "Found storage box '#{storage_box}', creating backups in data/backups..."
