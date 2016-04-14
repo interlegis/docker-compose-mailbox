b1c0d8ddb6e16d5c1fb28247974e2c931d5ab8c9
Fabio Rauber <fabiorauber@gmail.com>
2016-04-14 15:07:36 -0300

    Added option to create default domain administrator user

--- webserver/vimbadmin/runtime_setup.sh
+++ webserver/vimbadmin/runtime_setup.sh
@@ -31,22 +31,30 @@ mysql -u root -ppassword -h mysql vimbadmin -e "" &> /dev/null
 if [ $? -eq 0 ]; then
   echo "Using existing DB"
 else
-  echo "Creating DB and Superuser"
-  mysql -u root -ppassword -h mysql < /tmp/vimbadmin/db_setup.sql &> /dev/null && \
-
-  if [ -z "$MAIL_MAX_QUOTA" ]; then 
+  if [ -z "$MAIL_MAX_QUOTA" ]; then
     MAIL_MAX_QUOTA=0
-  else 
+  else
     MAIL_MAX_QUOTA=$(( $MAIL_MAX_QUOTA * 1024 * 1024 ))
-  fi 
-  if [ -z "$MAIL_MAX_MBOXES" ]; then 
-    MAIL_MAX_MBOXES=0; 
+  fi
+  if [ -z "$MAIL_MAX_MBOXES" ]; then
+    MAIL_MAX_MBOXES=0;
   fi
 
+  echo "Setting up DB and initial configuration..."
+
+  mysql -u root -ppassword -h mysql < /tmp/vimbadmin/db_setup.sql &> /dev/null && \
   HASH_PASS=`php -r "echo password_hash('$VIMBADMIN_SUPERUSER_PASSWORD', PASSWORD_DEFAULT);"`
   cd $vimbadmin_install_path && ./bin/doctrine2-cli.php orm:schema-tool:create && \
   mysql -u vimbadmin -ppassword -h mysql vimbadmin -e \
     "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('$VIMBADMIN_SUPERUSER', '$HASH_PASS', 1, 1, NOW(), NOW()); INSERT INTO domain (domain, max_quota, quota, max_mailboxes, transport, active, created, modified) VALUES ('$MAIL_DOMAIN', '$MAIL_MAX_QUOTA', '$MAIL_MAX_QUOTA', '$MAIL_MAX_MBOXES', 'virtual', 1, NOW(), NOW());" && \
-  echo "Vimbadmin setup completed successfully"
+  echo "Vimbadmin DB and Superuser setup completed successfully." 
+
+  if [ -n "$VIMBADMIN_DOMAINADMIN_USER" ] && [ -n "$VIMBADMIN_DOMAINADMIN_PASSWORD" ]; then
+     echo "Creating admin of the default mail domain..."
+     HASH_ADMPASS=`php -r "echo password_hash('$VIMBADMIN_DOMAINADMIN_PASSWORD', PASSWORD_DEFAULT);"`
+     mysql -u vimbadmin -ppassword -h mysql vimbadmin -e \
+    "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('$VIMBADMIN_DOMAINADMIN_USER', '$HASH_ADMPASS', 0, 1, NOW(), NOW()); INSERT INTO domain_admins (Admin_id, Domain_id) VALUES (2,1);" && \
+     echo "Domain administrator for $MAIL_DOMAIN created successfully."
+  fi
 fi
 
