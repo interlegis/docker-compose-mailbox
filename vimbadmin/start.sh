2b399a765e52a8f4796e42257f9a77cceb8fdcd6
Fabio Rauber <fabiorauber@gmail.com>
2016-10-04 16:28:44 -0300

    Allows for vimbadmnin to coexist inside a single mariadb instance

--- vimbadmin/start.sh
+++ vimbadmin/start.sh
@@ -3,7 +3,7 @@
 mysqlcheck() {
   # Wait for MySQL to be available...
   COUNTER=20
-  until mysql -h mysql -u $MYSQL_ENV_MYSQL_USER -p$MYSQL_ENV_MYSQL_PASSWORD -e "show databases" 2>/dev/null; do
+  until mysql -h mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -e "show databases" 2>/dev/null; do
     echo "WARNING: MySQL still not up. Trying again..."
     sleep 10
     let COUNTER-=1
@@ -13,7 +13,7 @@ mysqlcheck() {
     fi
   done
 
-  count=`mysql -h mysql -u $MYSQL_ENV_MYSQL_USER -p$MYSQL_ENV_MYSQL_PASSWORD -e "select count(*) from information_schema.tables where table_type='BASE TABLE' and table_schema='$MYSQL_ENV_MYSQL_DATABASE';" | tail -1`
+  count=`mysql -h mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -e "select count(*) from information_schema.tables where table_type='BASE TABLE' and table_schema='vimbadmin';" | tail -1`
   if [ "$count" == "0" ]; then
     echo "Database is empty. Creating database..."
     createdb
@@ -30,18 +30,22 @@ createdb() {
     MAIL_MAX_MBOXES=0;
   fi
 
+  mysql -u root -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD} -h mysql -e \
+    "CREATE DATABASE vimbadmin;GRANT ALL ON vimbadmin.* TO vimbadmin IDENTIFIED BY '${MYSQL_ENV_MYSQL_ROOT_PASSWORD}';FLUSH PRIVILEGES;"
+  sed -i -e "s %DB_PASSWORD% ${MYSQL_ENV_MYSQL_ROOT_PASSWORD} g" ${INSTALL_PATH}/application/configs/application.ini
+
   echo "Setting up DB and initial configuration..."
 
   HASH_PASS=`php -r "echo password_hash('$VIMBADMIN_SUPERUSER_PASSWORD', PASSWORD_DEFAULT);"`
   cd $INSTALL_PATH && ./bin/doctrine2-cli.php orm:schema-tool:create && \
-  mysql -u ${MYSQL_ENV_MYSQL_USER} -p${MYSQL_ENV_MYSQL_PASSWORD} -h mysql ${MYSQL_ENV_MYSQL_DATABASE} -e \
+  mysql -u vimbadmin -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD} -h mysql vimbadmin -e \
     "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('$VIMBADMIN_SUPERUSER', '$HASH_PASS', 1, 1, NOW(), NOW()); INSERT INTO domain (domain, max_quota, quota, max_mailboxes, transport, active, created, modified) VALUES ('$MAIL_DOMAIN', '$MAIL_MAX_QUOTA', '$MAIL_MAX_QUOTA', '$MAIL_MAX_MBOXES', 'virtual', 1, NOW(), NOW());" && \
   echo "Vimbadmin DB and Superuser setup completed successfully." 
 
   if [ -n "$VIMBADMIN_DOMAINADMIN_USER" ] && [ -n "$VIMBADMIN_DOMAINADMIN_PASSWORD" ]; then
      echo "Creating admin of the default mail domain..."
      HASH_ADMPASS=`php -r "echo password_hash('$VIMBADMIN_DOMAINADMIN_PASSWORD', PASSWORD_DEFAULT);"`
-     mysql -u ${MYSQL_ENV_MYSQL_USER} -p${MYSQL_ENV_MYSQL_PASSWORD} -h mysql ${MYSQL_ENV_MYSQL_DATABASE} -e \
+     mysql -u vimbadmin -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD} -h mysql vimbadmin -e \
     "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('$VIMBADMIN_DOMAINADMIN_USER', '$HASH_ADMPASS', 0, 1, NOW(), NOW()); INSERT INTO domain_admins (Admin_id, Domain_id) VALUES (2,1);" && \
      echo "Domain administrator for $MAIL_DOMAIN created successfully."
   fi
